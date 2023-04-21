7ff8b66e257f768e469486f8adad030cf0832434
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2023-04-21 10:56:12 +0200

    Code refactored

--- src/JusticeApp.Tests/APreChargeDecision.cs
+++ src/JusticeApp.Tests/APreChargeDecision.cs
@@ -3,20 +3,6 @@ using Xunit;
 
 namespace JusticeApp.Tests;
 
-public class APreChargeDecisionFixture
-{
-    public Suspect Suspect { get; }
-    public PreChargeDecision PreChargeDecision { get; }
-    public OffenceAdvice OffenceAdvice { get; }
-
-    public APreChargeDecisionFixture()
-    {
-        Suspect = new Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC);
-        PreChargeDecision = new PreChargeDecision();
-        OffenceAdvice = new OffenceAdvice();
-    }
-}
-
 public class APreChargeDecision : IClassFixture<APreChargeDecisionFixture>
 {
     private readonly Suspect _suspect;
---
+++ src/JusticeApp.Tests/APreChargeDecisionFixture.cs
@@ -0,0 +1,17 @@
+using HarmelLaw.JusticeApp;
+
+namespace JusticeApp.Tests;
+
+public class APreChargeDecisionFixture
+{
+    public Suspect Suspect { get; }
+    public PreChargeDecision PreChargeDecision { get; }
+    public OffenceAdvice OffenceAdvice { get; }
+
+    public APreChargeDecisionFixture()
+    {
+        Suspect = new Suspect(CriminalOffence.CUTTING_AWAY_BUOYS_ETC);
+        PreChargeDecision = new PreChargeDecision();
+        OffenceAdvice = new OffenceAdvice();
+    }
+}
